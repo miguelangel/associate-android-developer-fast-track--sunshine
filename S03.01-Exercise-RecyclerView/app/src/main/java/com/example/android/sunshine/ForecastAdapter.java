3339f74bbf5788807d2308314fb5762f0e3fe76e
Miguel Ángel <miguelangel.mnezfdez@gmail.com>
2017-02-11 16:13:31 +0100

    Complete S03.01-Exercise-RecyclerView

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,59 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by miguelangel on 11/2/17.
+ */
+
+public class ForecastAdapter
+        extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    public void setWeatherData (String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent,
+            int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        String weatherForThisDay = mWeatherData[position];
+        holder.mWeatherTextView.setText(weatherForThisDay);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWeatherData == null ? 0 : mWeatherData.length;
+    }
+
+    class ForecastAdapterViewHolder extends  RecyclerView.ViewHolder {
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View itemView) {
+            super(itemView);
+            mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+        }
+
+    }
+}
